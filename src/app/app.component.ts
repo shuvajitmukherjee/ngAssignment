b0cce2fbc0b8a1914e60d237af7f9b64e11e0c21
shuvajitmukherjee <shuvajit.mukherjee@learningmate.com>
2018-07-06 12:41:12 +0530

    Changes for assignment

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { contentService } from '../services/content.services'
+
+// interface ContentsType {
+//   title: string,
+//   sfxPool: {'@value':string}
+// }
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers: [contentService]
 })
-export class AppComponent {
-  title = 'app';
+
+export class AppComponent implements OnInit {
+  contents: any = {};
+  constructor(private _contentService: contentService) { }
+  ngOnInit() {
+    this._contentService.getContent()
+      .subscribe(resContent => {
+        this.contents = resContent;
+        console.log(this.contents);
+      });
+
+  }
 }
