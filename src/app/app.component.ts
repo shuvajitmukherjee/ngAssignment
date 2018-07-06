0dd8ddc5074a18d60f08bb1967193999f23920d0
sandipta roy <sandipta.roy@learningmate.com>
2018-07-06 15:40:34 +0530

    angular app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -15,13 +15,36 @@ import { contentService } from '../services/content.services'
 
 export class AppComponent implements OnInit {
   contents: any = {};
+  currentRound: string = "round1";
+  currentQuestion: number = 0;
+  selectedAnswerIndex: number = 0;
+  okButtonDisabled: boolean = true;
+  numberOfQuestionsInCurrentRound: number;
   constructor(private _contentService: contentService) { }
   ngOnInit() {
     this._contentService.getContent()
       .subscribe(resContent => {
         this.contents = resContent;
-        console.log(this.contents);
+        this.numberOfQuestionsInCurrentRound = this.contents.rounds[this.currentRound].questions.length;        
       });
-
+  }
+  optionSelected(index) {
+    this.okButtonDisabled = false;
+    this.selectedAnswerIndex = index;
+  }
+  okClicked() {
+    if (this.contents.rounds[this.currentRound].questions[this.currentQuestion].correctAnswer - 1 == this.selectedAnswerIndex) {
+      if(this.numberOfQuestionsInCurrentRound === (this.selectedAnswerIndex + 1)){ 
+        let currentRoundCalculation = this.currentRound.replace("round",'');
+        this.currentRound = (Object.keys(this.contents.rounds).length == parseInt(currentRoundCalculation))?"round1":"round"+(parseInt(currentRoundCalculation)+1);
+        this.currentQuestion = 0;         
+      }else{
+        this.currentQuestion += 1; 
+      }
+      this.selectedAnswerIndex = 0;
+      console.log("correct");
+    } else {
+      console.log("Wrong");
+    }       
   }
 }
