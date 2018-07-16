76f10e8364f0b13ad84f25f7ad281743dc8c89c7
sandipta roy <sandipta.roy@learningmate.com>
2018-07-16 18:55:17 +0530

    change image logic changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,7 @@
-import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Component, OnInit, OnDestroy, ViewChildren, QueryList, ElementRef } from '@angular/core';
 import { contentService } from '../services/content.services'
 
-// interface ContentsType {
+// interface ContentsType {s
 //   title: string,
 //   sfxPool: {'@value':string}
 // }
@@ -23,7 +23,26 @@ export class AppComponent implements OnInit {
   currImageName: string = "/assets/img/lock0001.png";
   setintervalHolder: any;
   changeDivColor: number = 0;
+  clickedCounter: number = 0;
+  cameFrom: number;
+  imageSet: any = {
+    "round1": [
+      { 'start': 1, 'end': 46 },
+      { 'start': 47, 'end': 138 }
+    ],
+    "round2": [
+      { 'start': 139, 'end': 184 },
+      { 'start': 185, 'end': 230 },
+      { 'start': 231, 'end': 276 }
+    ],
+    "round3": [
+      { 'start': 277, 'end': 322 },
+      { 'start': 323, 'end': 368 },
+      { 'start': 369, 'end': 456 }
+    ]
+  }
 
+  @ViewChildren('options') optionBtnElement: QueryList<ElementRef>
   constructor(private _contentService: contentService) { }
   ngOnInit() {
     this._contentService.getContent()
@@ -40,10 +59,23 @@ export class AppComponent implements OnInit {
     this.selectedAnswerIndex = index;
   }
   okClicked() {
-    if (this.contents.rounds[this.currentRound].questions[this.currentQuestion].correctAnswer - 1 == this.selectedAnswerIndex) {
-      this.changeImage();
+    if (this.contents.rounds[this.currentRound].questions[this.currentQuestion].status === 2) {   
+      this.contents.rounds[this.currentRound].questions[this.currentQuestion]['status'] = 3;   
+      this.changeQuestion();
     } else {
-      this.playTestPassed("Incorrect");
+      if (this.contents.rounds[this.currentRound].questions[this.currentQuestion].correctAnswer - 1 == this.selectedAnswerIndex) {
+        this.clickedCounter = 0;
+        this.changeImage();
+      } else {
+        this.playTestPassed("Incorrect");
+        this.okButtonDisabled = true;
+
+        if (this.clickedCounter == 1) {
+          this.showCorrectAnswer();
+        } else {
+          this.clickedCounter += 1;
+        }
+      }
     }
   }
 
@@ -54,35 +86,105 @@ export class AppComponent implements OnInit {
 
   }
 
+  
+
+  getCurrentRoundNextQuestion(): any {
+    // this.contents.rounds[this.currentRound].questions.find((row, index) => {
+    //   console.log(row.status)
+    //   if (row.status !== 1) {
+    //     return index;
+    //   }
+    // });
+
+    let result;
+    this.contents.rounds[this.currentRound].questions.sort((prevRow,nextRow)=>{
+      return prevRow.status > 0;
+    });
+    
+    for (let i = 0; i < this.contents.rounds[this.currentRound].questions.length; i++) {
+      if (this.contents.rounds[this.currentRound].questions[i].status !== 1) {
+        result = i;
+        break;
+      }
+    }
+    return result;
+
+
+  }
+
+
+  changeQuestion() {
+     clearTimeout(this.setintervalHolder);
+    // cases if all are  
+    let getNextQuestion = this.getCurrentRoundNextQuestion();
+    if (typeof getNextQuestion !== 'undefined') {
+      this.okButtonDisabled = true;
+      this.currentQuestion = getNextQuestion;
+    } else {
+      // change round   
+      let currentRoundCalculation = this.currentRound.replace("round", '');
+      if (Object.keys(this.contents.rounds).length != parseInt(currentRoundCalculation)) {
+        this.currentRound = "round" + (parseInt(currentRoundCalculation) + 1);
+        this.currentQuestion = 0;
+      } else {
+        this.okButtonDisabled = true;
+      }
+    }
+
+
+    /*
+    
+    
+    
+        let shiftQuestionTo = this.checkCurrentRoundFailedQuestions();
+        if (shiftQuestionTo !== 'undefined') {
+          this.okButtonDisabled = true;
+          this.currentQuestion = shiftQuestionTo;
+        } else {
+          let completedQuestionsInRound = this.getCompletedQuestions();
+          if (this.numberOfQuestionsInCurrentRound === (this.currentQuestion + 1)) {
+    
+            let currentRoundCalculation = this.currentRound.replace("round", '');
+            if (Object.keys(this.contents.rounds).length != parseInt(currentRoundCalculation)) {
+              this.currentRound = (Object.keys(this.contents.rounds).length == parseInt(currentRoundCalculation)) ? "finished" : "round" + (parseInt(currentRoundCalculation) + 1);
+              this.numberOfQuestionsInCurrentRound = this.contents.rounds[this.currentRound].questions.length;
+              this.currentQuestion = 0;
+            } else {
+              this.okButtonDisabled = true;
+            }
+          } else {
+            this.okButtonDisabled = true;
+            this.currentQuestion += 1;
+          }
+        }*/
+        this.resetOption();
+  }
+
   changeImage() {
-    let curVal = this.contents.rounds[this.currentRound].questions[this.currentQuestion].currentValue,
-      lastVal = this.contents.rounds[this.currentRound].questions[this.currentQuestion].LastValue;
+    let totalQuestions = this.contents.rounds[this.currentRound].questions.length;  
+    let alreadyAnswered = this.contents.rounds[this.currentRound].questions.filter((row,index)=>{
+      if(row.status === 1){
+        return row;
+      }
+    });
+    let indexOfImage = (typeof alreadyAnswered === 'undefined')?0:alreadyAnswered.length;
+    let curVal =  this.imageSet[this.currentRound][indexOfImage].start,
+      lastVal = this.imageSet[this.currentRound][indexOfImage].end;
+      this.contents.rounds[this.currentRound].questions[this.currentQuestion]['status'] = 1;      
     this.rotateImage(curVal, lastVal);
   }
+
+
   rotateImage(curVal, lastVal) {
     this.currImageName = `/assets/img/lock${this.padDigits(curVal, 4)}.png`;
     if (curVal != lastVal) {
       this.setintervalHolder = setTimeout(() => {
         this.rotateImage(parseInt(curVal) + 1, lastVal);
-      }, 100);
+      }, 50);
     } else {
-      clearTimeout(this.setintervalHolder);
-      if (this.numberOfQuestionsInCurrentRound === (this.currentQuestion + 1)) {
-        let currentRoundCalculation = this.currentRound.replace("round", '');
-
-        if (Object.keys(this.contents.rounds).length != parseInt(currentRoundCalculation)) {
-          this.currentRound = (Object.keys(this.contents.rounds).length == parseInt(currentRoundCalculation)) ? "finished" : "round" + (parseInt(currentRoundCalculation) + 1);
-          this.numberOfQuestionsInCurrentRound = this.contents.rounds[this.currentRound].questions.length;
-          this.currentQuestion = 0;
-        } else {         
-          this.okButtonDisabled = true;
-        }
-      } else {
-        this.okButtonDisabled = true;
-        this.currentQuestion += 1;
-      }
       this.selectedAnswerIndex = -1;
       this.playTestPassed("Correct");
+      this.changeQuestion();
     }
   }
 
@@ -93,9 +195,18 @@ export class AppComponent implements OnInit {
   padDigits(number, digits) {
     return Array(Math.max(digits - String(number).length + 1, 0)).join('0') + number;
   }
-  
-  playSwf(){
-    let newWin = window.open("","_blank");
+
+  playSwf() {
+    let newWin = window.open("", "_blank");
     newWin.document.getElementsByTagName('body')[0].innerHTML = `<object width="400" height="50" data="localhost:4201/assets/img/hlp.swf"></object>`;
+  }
+
+  showCorrectAnswer() {
+    this.clickedCounter = 0;
+    this.contents.rounds[this.currentRound].questions[this.currentQuestion]['status'] = 2;
+    let el = this.optionBtnElement["_results"][this.contents.rounds[this.currentRound].questions[this.currentQuestion].correctAnswer - 1].nativeElement as HTMLElement;
+    el.click();
+    this.okButtonDisabled = true;
+    setTimeout(()=>this.okClicked(),3000);
   }
 }
